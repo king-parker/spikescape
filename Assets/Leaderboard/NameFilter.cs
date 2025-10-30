fa5e859ff6258cb57c4368adbbe93ef975f187fe
Parker King <kingparker.a@gmail.com>
2025-10-29 22:30:55 -0600

    Add name filter

---
+++ Assets/Leaderboard/NameFilter.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Spikescape.Leaderboard
+{
+    public class NameFilter : MonoBehaviour
+    {
+        public static NameFilter Instance;
+
+        public bool IsReady { get; private set; }
+
+        [SerializeField] private string bannedWordsFileName = "en-bad-words";
+
+        private HashSet<string> _bannedWords;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            _ = LoadBannedWordsAsync();
+        }
+
+        public bool IsNameValid(string playerName)
+        {
+            if (!IsReady)
+            {
+                Debug.LogError("NameFilter accessed before it is ready.");
+                throw new System.Exception("NameFilter accessed before it is ready.");
+            }
+
+            var nameWords = playerName
+                .ToLowerInvariant()
+                .Split(new[] { ' ', '-', '_'}, System.StringSplitOptions.RemoveEmptyEntries)
+                .Select(w => w.Trim());
+
+            foreach (var word in nameWords)
+            {
+                if (_bannedWords.Contains(word))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private async Task LoadBannedWordsAsync()
+        {
+            TextAsset file = Resources.Load<TextAsset>(bannedWordsFileName);
+
+            if (file != null)
+            {
+                var words = file.text
+                    .Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries)
+                    .Select(w => w.Trim().ToLowerInvariant());
+                _bannedWords = new HashSet<string>(words);
+            }
+            else
+            {
+                Debug.LogError($"Failed to load banned words file. File {bannedWordsFileName}.txt not found.");
+                _bannedWords = new HashSet<string>();
+            }
+
+            await Task.Yield();
+        }
+    }
+}
